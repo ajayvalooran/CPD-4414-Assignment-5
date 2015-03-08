72969789d07599e6b043e51a08128dea9068d188
c0647711 <ajayvalooran@gmail.com>
2015-03-07 19:10:47 -0500

    initial commit

---
+++ src/java/test/products.java
@@ -0,0 +1,65 @@
+package test;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author c0647711
+ */
+@Path("/products")
+public class products {
+
+
+
+    @GET
+    @Produces("application/json")
+    protected String doGet() {
+        
+        
+         String result = getResults("SELECT * FROM PRODUCT");
+         return result;
+    }
+
+    /**
+     * Provides POST /servlet?name=XXX&age=XXX
+     *
+     * @param request - the request object
+     * @param response - the response object
+     */
+   
+    private String getResults(String query, String... params) {
+        StringBuilder sb = new StringBuilder();
+        try (Connection conn = credentials.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                sb.append(String.format("%s\t%s\t%s\t%s\n", rs.getInt("product_id"), rs.getString("product_name"), rs.getString("product_description"), rs.getInt("quantity")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sb.toString();
+    }
+
+    
+}
